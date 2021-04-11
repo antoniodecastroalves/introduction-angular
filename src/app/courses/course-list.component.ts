5ddf94b413a5b7731db60535fc30ed77637d5eea
Antonio Alves <antoniodecastroalves@gmail.com>
2021-04-10 23:46:31 -0300

    01.06 - Creating components with interdependent attributes

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -15,22 +15,32 @@ export class CourseListComponent implements OnInit {
       {
         id: 1,
         name: 'Angular : Forms',
-        imageUrl: '',
+        imageUrl: '/assets/images/forms.png',
         price: 99.99,
         code: 'ANF-9687',
         duration: 120,
-        rating: 5.4,
+        rating: 4.4,
         releaseDate: 'April, 9, 2021',
       },
       {
         id: 2,
         name: 'Angular : HTTP',
-        imageUrl: '',
+        imageUrl: '/assets/images/http.png',
         price: 45.99,
         code: 'ANH-9025',
         duration: 80,
         rating: 4.7,
         releaseDate: 'December, 2, 2020',
+      },
+      {
+        id: 3,
+        name: 'Angular : Routes',
+        imageUrl: '/assets/images/router.png',
+        price: 37.97,
+        code: 'ANH-9025',
+        duration: 60,
+        rating: 4.0,
+        releaseDate: 'November, 14, 2020',
       }
     ]
 
