4ce52f9b94e6deb4f2421310d791a99ff04c63cc
Antonio Alves <antoniodecastroalves@gmail.com>
2021-04-12 00:52:55 -0300

    02.02 - Handling data binding events

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -9,13 +9,26 @@ import { CourseService } from './course.service';
 
 export class CourseListComponent implements OnInit {
 
-  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+
+  _courses: Course[] = [];
+
+  _filterBy: string;
 
   constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
+    this._courses = this.courseService.retrieveAll();
+    this.filteredCourses = this._courses;
+  }
 
-    this.courses = this.courseService.retrieveAll();
+  set filter(value: string) {
+    this._filterBy = value;
 
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+  }
+
+  get filter() {
+    return this._filterBy;
   }
 }
