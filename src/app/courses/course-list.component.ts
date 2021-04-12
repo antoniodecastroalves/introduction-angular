6bafdd6754d249d93ee92654a0eed80b588ee73c
Antonio Alves <antoniodecastroalves@gmail.com>
2021-04-11 23:39:06 -0300

    02.01 - What is dependency injection

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -10,39 +11,11 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService) { }
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular : Forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 99.99,
-        code: 'ANF-9687',
-        duration: 120,
-        rating: 4.4,
-        releaseDate: 'April, 9, 2021',
-      },
-      {
-        id: 2,
-        name: 'Angular : HTTP',
-        imageUrl: '/assets/images/http.png',
-        price: 45.99,
-        code: 'ANH-9025',
-        duration: 80,
-        rating: 4.7,
-        releaseDate: 'December, 2, 2020',
-      },
-      {
-        id: 3,
-        name: 'Angular : Routes',
-        imageUrl: '/assets/images/router.png',
-        price: 37.97,
-        code: 'ANH-9025',
-        duration: 60,
-        rating: 4.0,
-        releaseDate: 'November, 14, 2020',
-      }
-    ]
+
+    this.courses = this.courseService.retrieveAll();
 
   }
 }
